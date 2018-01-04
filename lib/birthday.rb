ac6dadb07d2a727eff74734d8ffb98010faa9658
tal shachar <tal.shachar@buzzfeed.com>
2018-01-04 05:15:44 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,8 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+ birthday_kids = {
+   "Timmy" => 9,
+   "Sarah" => 6,
+   "Amanda" => 27
+ }
 
 def happy_birthday(birthday_kids)
   def happy_birthday(birthday_kids)
@@ -11,5 +11,3 @@ def happy_birthday(birthday_kids)
     end
   end
 end
-
-
